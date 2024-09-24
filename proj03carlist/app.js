247aa185f5bdb95085f8f0f1735156f1fbd76afa
comstudyschool <comstudyschool@gmail.com>
2024-09-24 16:52:08 +0900

    car list crud 구현

--- proj03carlist/app.js
+++ proj03carlist/app.js
@@ -4,9 +4,15 @@ const app = express();
 const path = require("path");
 const static = require("serve-static");
 const router = express.Router();
+const bodyParser = require("body-parser");
 
 app.set('port', 3000);
+app.set("views", path.join(__dirname, "views") ); // prefix (ejs 페이지 경로)
+app.set("view engine", "ejs"); // suffix (확장자)
+
 app.use("/", static(path.join(__dirname, "public") ) );
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 const carList = [
     {_id:1001, name:"GRANDEUR", price:3500, company:"HYUNDAI", year:2019},
@@ -16,20 +22,28 @@ const carList = [
 ];
 // 목록
 router.route("/car/list").get((req, res)=>{
-    req.app.render('car/list',{}, (err, html) => {
+    req.app.render('car/list',{carList}, (err, html) => {
         if (err) throw err;
         res.end(html);
     });
 });
 // 입력
 router.route("/car/input")
-.get((req, res)=>{
-    req.app.render('car/input',{}, (err, html) => {
-        if (err) throw err;
-        res.end(html);
+    .get((req, res)=>{
+        req.app.render('car/input',{}, (err, html) => {
+            if (err) throw err;
+            res.end(html);
+        });
+    })
+    .post((req, res)=>{
+        // 일머리가 중요해!!!
+        // 파라미터 값 가져오기
+        // 가져온 값 확인
+        // 리스트에 객체로 추가
+        // 목록으로 redirect
+        console.log(req.body);
+        res.redirect("/car/list");
     });
-})
-.post();
 // 상세 보기
 router.route("/car/detail")
 .get((req, res)=>{
